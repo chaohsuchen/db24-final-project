60051549332fd562893f374cf581c42115e92382
Chao-Hsu <joeychen89@gmail.com>
2024-06-16 20:14:53 +0800

    SIMD for Int8 Analysis Done

--- core-patch/src/main/java/org/vanilladb/core/sql/distfn/IntEuclideanFn.java
+++ core-patch/src/main/java/org/vanilladb/core/sql/distfn/IntEuclideanFn.java
@@ -9,6 +9,38 @@ public class IntEuclideanFn extends IntDistanceFn {
         super(fld);
     }
 
+    // SIMD method: We don't use SIMD here! Since it is int 8, we need to iterate
+    // through the vector first
+    // before we can do SIMD, this cause a performance loss.
+
+    // @Override
+    // protected int calculateDistance(Int8VectorConstant vec) {
+    // int[] queryArray = query.stream().mapToInt(Integer::intValue).toArray();
+    // int[] vecArray = vec.stream().mapToInt(Integer::intValue).toArray();
+
+    // int length = vec.dimension();
+    // int sum = 0;
+    // int i = 0;
+
+    // // SIMD calculation
+    // for (; i < SPECIES.loopBound(length); i += SPECIES.length()) {
+    // var m = SPECIES.indexInRange(i, length);
+    // var queryVector = IntVector.fromArray(SPECIES, queryArray, i, m);
+    // var vecVector = IntVector.fromArray(SPECIES, vecArray, i, m);
+    // var diff = queryVector.sub(vecVector);
+    // var squaredDiff = diff.mul(diff);
+    // sum += squaredDiff.reduceLanesToInt(VectorOperators.ADD);
+    // }
+
+    // // Tail loop for remaining elements
+    // for (; i < length; i++) {
+    // int diff = queryArray[i] - vecArray[i];
+    // sum += diff * diff;
+    // }
+
+    // return sum;
+    // }
+
     @Override
     protected int calculateDistance(Int8VectorConstant vec) {
         int sum = 0;
@@ -18,11 +50,12 @@ public class IntEuclideanFn extends IntDistanceFn {
         }
         return sum;
     }
+
     @Override
     protected double calculateDistance(VectorConstant vec) {
         double sum = 0;
         for (int i = 0; i < vec.dimension(); i++) {
-            double diff = query.get(i) - (float)vec.get(i);
+            double diff = query.get(i) - (float) vec.get(i);
             sum += diff * diff;
         }
         return sum;
